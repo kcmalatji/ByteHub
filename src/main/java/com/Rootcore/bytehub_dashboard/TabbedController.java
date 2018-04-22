94ea144cff52abb29e4657e46456ae8fc6216ea6
chris <you@example.com>
2018-04-22 22:20:00 +0200

    get data works

--- src/main/java/com/Rootcore/bytehub_dashboard/TabbedController.java
+++ src/main/java/com/Rootcore/bytehub_dashboard/TabbedController.java
@@ -16,7 +16,6 @@ public class TabbedController {
         connect=DriverManager.getConnection("jdbc:mysql://localhost:3306/bytehub_dashboard?autoReconnect=true&useSSL=false&"+"user=root&password=root");
         statement= connect.createStatement();
         resultSet=statement.executeQuery("select * from bytehub_dashboard.gateway_events");
-//        writeResultSet(resultSet);
        Timestamp timestamp=new Timestamp(System.currentTimeMillis());
         preparedStatement=connect.prepareStatement("insert into  bytehub_dashboard.gateway_events values (default ,?,?,?,?,?,?)");
     preparedStatement.setString(1,"test1");
@@ -24,10 +23,42 @@ public class TabbedController {
     preparedStatement.setString(3,"test1");
     preparedStatement.setString(4,"test1");
     preparedStatement.setString(5,"test1");
-//    preparedStatement.setTimestamp(6,new java.sql.Timestamp(1400,04,12,9,12,56,0));
+
     preparedStatement.setTimestamp(6,new java.sql.Timestamp(timestamp.getTime()));
     preparedStatement.executeUpdate();
-        System.out.println("information added");
+
+
+
+    resultSet=statement.executeQuery("select * from bytehub_dashboard.gateway_events");
+    writeMetaData(resultSet);
+        writeResultset(resultSet);
+        System.out.println(resultSet);
+
+    System.out.println("information added");
+    }
+    public void writeMetaData(ResultSet resultSet) throws SQLException{
+        System.out.println("the colums in the table are");
+        System.out.println("Table : "+ resultSet.getMetaData().getTableName(1) );
+        for (int i=1;i<=resultSet.getMetaData().getColumnCount();i++)
+        {
+            System.out.println("column "+ i +" "+resultSet.getMetaData().getColumnName(i));
+        }
+    }
+    public void writeResultset(ResultSet resultSet)throws SQLException{
+        while(resultSet.next()){
+             String bytehub_id=resultSet.getString("bytehub_id");
+             String package_id=resultSet.getString("package_id");
+             String route_id=resultSet.getString("route_id");
+             String query=resultSet.getString("query");
+             String cid=resultSet.getString("cid");
+             Timestamp created_t=resultSet.getTimestamp("created_t");
+            System.out.println("bytehub_id :"+ bytehub_id);
+            System.out.println("package_id :"+package_id);
+            System.out.println("route_id :"+route_id);
+            System.out.println("query :"+ query);
+            System.out.println("cid :"+ cid);
+            System.out.println("created_t :"+ created_t);
+        }
     }
 
     @GetMapping("/tabbedview")
